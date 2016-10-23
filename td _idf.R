57e5e3524070407c0780f56bb83d220eb5a3a7fd
Ryan Burge <ryanburge@gmail.com>
2016-10-22 22:11:08 -0500

    adding some graphs.

--- td _idf.R
+++ td _idf.R
@@ -8,9 +8,40 @@ tweets_words <- tweets_words  %>% bind_tf_idf(word, screenName, n) %>%
 arrange(desc(tf_idf)) %>%
 mutate(word = factor(word, levels = rev(unique(word))))
 
-ggplot(tweets_words  %>% filter(screenName == "JaySekulow") %>% top_n(10), aes(x=word, y= tf_idf)) +
+ggplot(tweets_words  %>% filter(screenName == "TedHaggard") %>% top_n(10), aes(x=word, y= tf_idf)) +
 geom_bar(aes(alpha = tf_idf),
 stat="identity",
 fill = "#4169E1") +
 coord_flip()
+
+ggplot(tweets_words  %>% top_n(100), aes(x=word, y= tf_idf)) +
+  geom_bar(aes(alpha = tf_idf),
+           stat="identity",
+           fill = "#4169E1") +
+  coord_flip()
+
+library(tidytext)
+library(viridis)
+
+total_words <- tweets_words %>% group_by(screenName) %>% summarize(total = sum(n))
+all_words <- left_join(tweets_words, total_words)
+all_words <- all_words %>% bind_tf_idf(word, screenName, n)
+
+all_words %>%
+select(-total) %>%
+arrange(desc(tf_idf))
+
+plot1 <- all_words %>%
+arrange(desc(tf_idf)) %>%
+mutate(word = factor(word, levels = rev(unique(word))))
+ggplot(plot1[1:20,], aes(tf_idf, word, fill = screenName, alpha = tf_idf)) +
+geom_barh(stat = "identity") +
+labs(title = "Highest tf-idf words in Jane Austen's Novels",
+y = NULL, x = "tf-idf") +
+theme_tufte(base_family = "Arial", base_size = 13, ticks = FALSE) +
+scale_alpha_continuous(range = c(0.6, 1), guide = FALSE) +
+scale_x_continuous(expand=c(0,0)) +
+scale_fill_viridis(end = 0.85, discrete=TRUE) +
+theme(legend.title=element_blank()) +
+theme(legend.justification=c(1,0), legend.position=c(1,0))
 
