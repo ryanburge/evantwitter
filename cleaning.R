f666ce226ed7af2376509931d25721400826daa3
Ryan Burge <ryanburge@gmail.com>
2016-12-11 17:27:05 -0600

    added a lot of comments. cleaned up ggplots.

--- cleaning.R
+++ cleaning.R
@@ -12,18 +12,39 @@ library(scales)
 library(reshape2)
 library(dplyr)
 library(RColorBrewer)
+library(ggstance)
+library(viridis)
 
 
 count <- count(tweets, screenName)
 count  %>% arrange(count, desc(n))
-count <- filter(count, n >1)
+count <- filter(count, n >100)
+
+## Which Users Tweet the Most
 ggplot(count, aes(x=reorder(screenName, n), y = n)) + geom_bar(stat="identity") + coord_flip()
+
+## Total Distribution of Tweets Across Time
+ggplot(tweets, aes(created)) + geom_histogram(aes(fill = ..count..), bins = 270) + 
+  xlab("Date Tweeted") + ylab("Total Number of Tweets")
+
+
+## Tweets by Day of the Week
+ggplot(data = tweets, aes(x = wday(created, label = TRUE))) +
+  geom_histogram(breaks = seq(0.5, 7.5, by =1), aes(fill = ..count..), stat = "count") +
+  theme(legend.position = "none") +
+  xlab("Day of the Week") + ylab("Number of tweets") + 
+  scale_fill_gradient(low = "midnightblue", high = "aquamarine4")
+
+## Tweets by Month
+ggplot(data = tweets, aes(x = month(created, label = TRUE))) +
+  geom_histogram(breaks = seq(0.5, 7.5, by =1), aes(fill = ..count..), stat = "count") +
+  theme(legend.position = "none") +
+  xlab("Day of the Week") + ylab("Number of tweets") + 
+  scale_fill_gradient(low = "midnightblue", high = "aquamarine4")
+
+## Stripping out the Tweets by Hours
+tweets$hours <- format(as.POSIXct(strptime(tweets$created,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%H:%M")
 tweets$created <-as.POSIXct(tweets$created)
-minutes <- 120
-ggplot(data=tweets, aes(x=created)) +
-geom_histogram(aes(fill=..count..), binwidth=160*minutes) +
-scale_x_datetime("Date") +
-scale_y_continuous("Frequency") + ggtitle("Usage of the #FundEIU Hashtag")
 
 
 nohandles <- str_replace_all(tweets$text, "@\\w+", "")
@@ -32,8 +53,9 @@ wordCorpus <- tm_map(wordCorpus, removePunctuation)
 wordCorpus <- tm_map(wordCorpus, content_transformer(tolower))
 wordCorpus <- tm_map(wordCorpus, removeWords, stopwords("english"))
 wordCorpus <- tm_map(wordCorpus, stripWhitespace)
+wordCorpus <- tm_map(wordCorpus, stemDocument)
 
-gonewords <- tm_map(wordCorpus, removeWords, c("amp", "jaylive", "18885675635", "'re", "periscope"))
+wordCorpus <- tm_map(wordCorpus, removeWords, c("amp", "jaylive", "18885675635", "'re", "periscope", "rt", "…", "1", "2", "4", "u", "3", "10", "7", "'s", "vom", "5"))
 
 library(RColorBrewer)
 pal <- brewer.pal(9,"YlGnBu")
