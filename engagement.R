a8d8f07c68833a6f1b5c60d261747a7726d9dea9
Ryan Burge <ryanburge@gmail.com>
2017-01-07 18:43:58 -0600

    engagement syntax.

--- engagement.R
+++ engagement.R
@@ -25,8 +25,38 @@ df <- rbind(notreply, withreply)
 follow <- read.csv("clean_sheet.csv")
 merge <- merge(df, follow, by=c("screenName"))
 
+write.csv(merge, "engage.csv")
+engage <-read.csv("engage.csv")
 
-ggplot(merge %>% filter(Followers >500000) , aes(x=reorder(screenName, -number), y= number, fill=label)) + 
-  geom_col(position="dodge") + theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(values = c("gray", "black")) + xlab("Screen Name") + ylab("Number of Tweets")  +     
-  theme(legend.position="bottom") + labs(fill="")
+ggplot(engage %>% filter(Followers >500000) , 
+       aes(x=reorder(screenName, -number), y= number, fill=label)) + 
+  geom_col(position="dodge") + theme_classic() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values = c("gray", "black")) + 
+  theme(legend.justification=c(.9,.825),legend.position=c(.9,.825)) +
+  theme(panel.grid.major.y=element_line(colour="grey50",linetype=2)) + 
+  xlab("Screen Name") + 
+  ylab("Number of Tweets") + labs(fill="") +
+  ggtitle("Evangelical Leaders' Level of Engagement with Twitter Followers",
+          subtitle = "Engagement measured by number of tweets\ncontaining '@.'")
+
+scatter <-read.csv("engage_scatter.csv")
+ggplot(scatter, aes(x=replyrate*100, y=followers/1000)) + geom_point(shape =1) + geom_smooth(method=lm) + theme_classic() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values = c("gray", "black")) + 
+  theme(legend.justification=c(.9,.825),legend.position=c(.9,.825)) +
+  theme(panel.grid.major.y=element_line(colour="grey50",linetype=2)) + 
+  xlab("Percent of Tweets That Contain '@'") + 
+  ylab("Number of Followers (in thousands)") + labs(fill="") +
+  ggtitle("Relationship Between Engagement and Popularity",
+          subtitle = "Engagement measured by number of tweets containing '@.'")
+
+ggplot(scatter, aes(x=replyrate*100, y=rtscore)) + geom_point(shape =1) + geom_smooth(method=lm) + theme_classic() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(values = c("gray", "black")) + 
+  theme(legend.justification=c(.9,.825),legend.position=c(.9,.825)) +
+  theme(panel.grid.major.y=element_line(colour="grey50",linetype=2)) + 
+  xlab("Percent of Tweets That Contain '@'") + 
+  ylab("Number of Retweets per Tweet") + labs(fill="") +
+  ggtitle("Relationship Between Engagement and Retweets",
+          subtitle = "Engagement measured by number of tweets containing '@.'")
